303d476842eb8cfeeca854a101db0b9a17e7b0e2
Eklerek <mwiczuk99@gmail.com>
2020-01-02 20:22:43 +0100

    New object isn't created each iteration of main loop

--- GaussMethod/main.cpp
+++ GaussMethod/main.cpp
@@ -3,46 +3,43 @@
 
 int main()
 {
-	std::string name = "test.csv";
+	std::string fileName = "test.csv";
+	Gauss gauss(fileName);
 
 	bool run = true;
 	while (run)
 	{
 		std::cout << "Menu:\n"
 			<< "1. Podstawowa metoda eliminacji Gaussa\n"
-			<< "2. Metoda eliminacji Gaussa z wyborem elementu podstawowego w wierszu\n"
-			<< "3. Metoda eliminacji Gaussa z wyborem elementu podstawowego w kolumnie\n"
+			<< "2. Metoda eliminacji Gaussa z zamiana wierszy\n"
+			<< "3. Metoda eliminacji Gaussa z zamiana kolumn\n"
 			<< "4. Metoda eliminacji Gaussa z pe³nym wyborem elementu podstawowego\n"
 			<< "5. Wyjscie\n";
 		int choice;
-		std::cout << "Wprowadz opcje: ";
+		std::cout << "\nWprowadz opcje: ";
 		std::cin >> choice;
 		switch (choice)
 		{
 		case 1:
 		{
-			Gauss gauss(name);
 			gauss.basicElimination();
 			std::cout << '\n';
 			break;
 		}
 		case 2:
 		{
-			Gauss gauss(name);
 			gauss.swapRowsElimination();
 			std::cout << '\n';
 			break;
 		}
 		case 3:
 		{
-			Gauss gauss(name);
 			gauss.swapColumnsElimination();
 			std::cout << '\n';
 			break;
 		}
 		case 4:
 		{
-			Gauss gauss(name);
 			gauss.completeElimination();
 			std::cout << '\n';
 			break;
